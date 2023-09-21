a54cc2688bc63f921b821a582778979ee8cf9273
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-09-21 19:17:03 -0300

    Update Integrador.cpp

--- Integrador.cpp
+++ Integrador.cpp
@@ -1,21 +1,23 @@
 #include "Integrador.h"
 
-Integrador::Integrador()
-{
+Integrador::Integrador(){
 }
 
-Integrador::~Integrador()
-{
+Integrador::~Integrador(){
 }
 
-Sinal *Integrador::processar(Sinal *sinalIN)
-{
+Sinal *Integrador::processar(Sinal *sinalIN){
     int comprimentoOUT = sinalIN->getComprimento();
     double *sequenciaOUT = new double[comprimentoOUT];
-    sequenciaOUT[0] = sinalIN->getSequencia()[0];
-    for (int i = 1; i < comprimentoOUT; i++)
-        sequenciaOUT[i] = sinalIN->getSequencia()[i] + sinalIN->getSequencia()[i - 1]; //funciona pois o index é crescente,
-    Sinal *sinalOUT = new Sinal(sequenciaOUT, comprimentoOUT);                         //logo sequenciaOUT[i] é calculado após
-    delete[] sequenciaOUT;                                                             //sequenciaOUT[i-1] já ter sido calculado.
+    
+    double soma = 0;
+    for (int i = 0; i < comprimentoOUT; i++){
+        soma += sinalIN->getSequencia()[i];
+        sequenciaOUT[i] = soma;
+    }
+    Sinal *sinalOUT = new Sinal(sequenciaOUT, comprimentoOUT);   
+    
+    delete[] sequenciaOUT;                                                             
     return sinalOUT;
 }
+

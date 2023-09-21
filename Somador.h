9c390b77d0d7f474ff38e7f84a01f639e61155a5
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-09-21 19:23:30 -0300

    Update Somador.h

--- Somador.h
+++ Somador.h
@@ -6,6 +6,6 @@ class Somador{
 public:
     Somador();
     virtual ~Somador();
-    Sinal *processar(Sinal *sinalIN1, Sinal *sinalIN2);
+    Sinal* processar(Sinal *sinalIN1, Sinal *sinalIN2);
 };
 #endif

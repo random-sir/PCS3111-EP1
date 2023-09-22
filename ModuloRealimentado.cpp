9c0292dbd89d0212a376cd32f5342a2d10d2844e
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-09-22 20:17:44 -0300

    Update ModuloRealimentado.cpp

--- ModuloRealimentado.cpp
+++ ModuloRealimentado.cpp
@@ -1,49 +1,48 @@
 #include "ModuloRealimentado.h"
 
-Sinal* criarSinalAPartirDeElemento(Sinal* sinalIN, int i){  //cria um sinal de comprimento unitário com o elemento [i] em sinalIN.getSequencia
-
+Sinal* ModuloRealimentado::getSinalEspecifico(Sinal* sinalIN, int i){ 
     double sequencia[1];
     sequencia[0] = sinalIN->getSequencia()[i];
     Sinal* sinalOUT = new Sinal(sequencia,1);
     return sinalOUT;
 }
 
-ModuloRealimentado::ModuloRealimentado(double ganho)
-{
-    somadorModuloRealimentado = new Somador;
+ModuloRealimentado::ModuloRealimentado(double ganho){
+    somadorModuloRealimentado = new Somador();
     inversorModuloRealimentado = new Amplificador(-1);
     pilotoModuloRealimentado = new Piloto(ganho);
 }
 
-ModuloRealimentado::~ModuloRealimentado()
-{
+ModuloRealimentado::~ModuloRealimentado(){
     delete somadorModuloRealimentado;
     delete inversorModuloRealimentado;
     delete pilotoModuloRealimentado;
 }
 
-Sinal *ModuloRealimentado::processar(Sinal *sinalIN)
-{
+Sinal *ModuloRealimentado::processar(Sinal *sinalIN){
     //inicializacao
     const double velocidadeInicial = 0;
     Sinal* diferenca = nullptr;
     Sinal* saida = nullptr;
     Sinal* saidaInvertida = nullptr;
+    double* sequenciaSaidaInvertida = new double[sinalIN->getComprimento()];
 
     //primeira iteracao
-    double* sequenciaSaidaInvertida = new double[sinalIN->getComprimento()];
-    sequenciaSaidaInvertida[0] = 0;
-    diferenca = criarSinalAPartirDeElemento(sinalIN,0);
+    sequenciaSaidaInvertida[0] = velocidadeInicial * (-1);
+    diferenca = getSinalEspecifico(sinalIN,0);
     saida = pilotoModuloRealimentado->processar(diferenca);
     delete diferenca;
-    
+
+    //iteracoes subsequentes
     for (int i = 1; i < sinalIN->getComprimento(); i++){
-        sequenciaSaidaInvertida = inversorModuloRealimentado->processar(saida[i-1]);
+        sequenciaSaidaInvertida[i] = ( inversorModuloRealimentado->processar(saida) )->getSequencia()[i-1];
         saidaInvertida = new Sinal(sequenciaSaidaInvertida, i+1); 
-        diferenca = somadorModuloRealimentado->processar(sinalIN, saidaInvertida);
+        diferenca = somadorModuloRealimentado->processar(saidaInvertida, sinalIN);
         delete saida;
         saida = pilotoModuloRealimentado->processar(diferenca);
-        
+        delete saidaInvertida;
+        delete diferenca;
     }
-    return nullptr;
+    delete[] sequenciaSaidaInvertida;
+    return saida;
 }

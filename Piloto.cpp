05c980ed873d689a7de66202a5cb84ef5c13a1e9
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-09-22 17:05:11 -0300

    Update Piloto.cpp

--- Piloto.cpp
+++ Piloto.cpp
@@ -1,21 +1,18 @@
 #include "Piloto.h"
 
-Piloto::Piloto(double ganho)
-{
+Piloto::Piloto(double ganho){
     amplificadorPiloto = new Amplificador(ganho);
     integradorPiloto = new Integrador;
 }
 
-Piloto::~Piloto()
-{
+Piloto::~Piloto(){
     delete amplificadorPiloto;
     delete integradorPiloto;
 }
 
-Sinal *Piloto::processar(Sinal *sinalIN)
-{
+Sinal* Piloto::processar(Sinal *sinalIN){
     //só deixei a lógica mais clara. AINDA EM REFORMA, PROVAVELMENTE AINDA POSSUI ERROS.
-    Sinal* sinalOUT = new Sinal(sinalIN, sinalIN->getComprimento());
+    Sinal* sinalOUT = new Sinal(sinalIN->getSequencia(), sinalIN->getComprimento());
     sinalOUT = amplificadorPiloto->processar(sinalOUT);
     sinalOUT = integradorPiloto->processar(sinalOUT);
     return sinalOUT;

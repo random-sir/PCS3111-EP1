08f44f0dc766c27c180ebc08fe09c4ba102194d4
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-09-22 20:59:12 -0300

    Create menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,26 @@
+include <iostream>
+include <cmath>
+include "Sinal.h"
+include "Amplificador.h"
+include "Somador.h"
+include "Derivador.h"
+include "Integrador.h"
+include "Piloto.h"
+include "ModuloRealimentado.h"
+using namespace std;
+
+Sinal* novoSinal();
+void novaOperacao(Sinal *sinalIN);
+
+{
+/*
+*implementacao da interface
+*/
+}
+
+Sinal* novoSinal(){
+  return nullptr;
+}
+void novaOperacao(Sinal *sinalIN){
+  return void;
+}

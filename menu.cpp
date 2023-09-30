2d52e7a8a87ae0b60a6b53c617c8ce163023f82a
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-09-30 18:31:06 -0300

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -13,30 +13,34 @@ using namespace std;
 Sinal* novoSinal();
 void novaOperacao(Sinal *sinalIN);
 
-void menu() {
-    /*
-     *implementacao da interface
-     */
+void menu(){
+    /*implementacao da interface*/
     int opcao;
     Sinal *sinalIN;
-    cout << "\tSimulink em C++\nQual simulacao voce gostaria de fazer?\n1) Piloto Automatico\n2) Sua propria sequencia de operacoes\nEscolha: ";
+    cout << "\tSimulink em C++"                      << endl
+         << "Qual simulacao voce gostaria de fazer?" << endl
+         << "1) Piloto Automatico"                   << endl
+         << "2) Sua propria sequencia de operacoes"  << endl
+         << "Escolha: ";
     cin >> opcao;
     cout << endl;
     sinalIN = novoSinal();
-    if (opcao == 1) {
+    
+    if (opcao == 1){
         double ganho;
-        cout << "Qual o ganho do acelerador?\ng = ";
+        cout << "Qual o ganho do acelerador?" << endl
+             << "g = ";
         cin >> ganho;
         cout << endl;
         ModuloRealimentado *pilotoAutomatico = new ModuloRealimentado(ganho);
         Sinal *sinalOUT = pilotoAutomatico->processar(sinalIN);
         Grafico *grafico = new Grafico("Velocidade do Carro", sinalOUT->getSequencia(), sinalOUT->getComprimento());
         grafico->plot();
+        delete sinalIN;
+        delete pilotoAutomatico;
         delete grafico;
         delete sinalOUT;
-    } else {
-        novaOperacao(sinalIN);
-    }
+    }else novaOperacao(sinalIN);
 }
 
 Sinal* novoSinal(){
@@ -54,7 +58,7 @@ Sinal* novoSinal(){
     if (opcao == 1){
         for (int n = 0; n < tamanhoMaximo; n++) 
             sequencia[n] = 5 + 3 * cos(n * (M_PI / 8));
-    }else if (opcao == 2){
+    } else if (opcao == 2){
         double constante;
         cout << "Qual o valor dessa constante?" << endl 
              << "C = ";
@@ -62,7 +66,7 @@ Sinal* novoSinal(){
         cout << endl;
         for (int n = 0; n < tamanhoMaximo; n++)
             sequencia[n] = constante;
-    }else{
+    } else{
         double inclinacao;
         cout << "Qual a inclinacao dessa rampa?" << endl
              << "a = ";
@@ -97,22 +101,19 @@ void novaOperacao(Sinal *sinalIN) {
         sinalOUT = amplificadorOperacao->processar(sinalIN);
         delete sinalIN;
         delete amplificadorOperacao;
-    }
-    if (opcao == 2){
+    } else if (opcao == 2){
         Somador *somadorOperacao = new Somador();
         cout << "Informe mais um sinal para ser somado." << endl;
         Sinal *sinalSomado = novoSinal();
         sinalOUT = somadorOperacao->processar(sinalIN, sinalSomado); 
         delete sinalIN;
         delete somadorOperacao;
-    }
-    if (opcao == 3){
+    } else if (opcao == 3){
         Derivador *derivadorOperacao = new Derivador();
         sinalOUT = derivadorOperacao->processar(sinalIN); 
         delete sinalIN;
         delete derivadorOperacao;
-    }
-    if (opcao == 4){
+    } else{
         Integrador *integradorOperacao = new Integrador();
         sinalOUT = integradorOperacao->processar(sinalIN);
         delete sinalIN;
@@ -127,7 +128,7 @@ void novaOperacao(Sinal *sinalIN) {
     cout << endl;
     if (opcao == 1){
         novaOperacao(sinalOUT); //Solucao recursiva
-    }else{
+    } else{
         Grafico *grafico = new Grafico("Resultado Final", sinalOUT->getSequencia(), sinalOUT->getComprimento());
         grafico->plot();
         delete grafico;

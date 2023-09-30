10060cab9b3b88041a659046a28078d1c5ac3c4e
PdrMzn <142683617+PdrMzn@users.noreply.github.com>
2023-09-30 18:59:14 -0300

    Update teste.cpp

--- teste.cpp
+++ teste.cpp
@@ -1,5 +1,50 @@
-/*FUNÇÕES PARA TESTAR AS CLASSES*/
+#include "Amplificador.h"
+#include "Derivador.h"
+#include "Grafico.h"
+#include "Integrador.h"
+#include "ModuloRealimentado.h"
+#include "Piloto.h"
+#include "Sinal.h"
+#include "Somador.h"
+#include <iostream>
+using namespace std;
 
-void teste(){
+/*FUNÇÕES PARA TESTAR AS CLASSES*/
+/*TESTE DE MENU FAREMOS USANDO EP < ENTRADA.TXT > SAIDA.TXT*/
+
+void testaSinal();
+void testaSomador();
+void testaDerivador();
+void testaIntegrador();
+void testaAmplificador();
+void testaPiloto();
+void testaModuloRealimentado();
+
+void testaSinal(){
     return void;
 }
+
+void testaSomador(){
+    return void;
+}
+
+void testaDerivador(){
+    return void;
+}
+
+void testaIntegrador(){
+    return void;
+}
+
+void testaAmplificador(){
+    return void;
+}
+
+void testaPiloto(){
+    return void;
+}
+
+void testaModuloRealimentado(){
+    return void;
+}
+
